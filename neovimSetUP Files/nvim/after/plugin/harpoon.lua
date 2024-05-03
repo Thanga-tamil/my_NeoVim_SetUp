42bfd8be6f9be105888c9e6fb21e6ab4ff8025ae
Thangatamil <117431100+Thanga-tamil@users.noreply.github.com>
2024-05-03 12:22:58 +0530

    Add files via upload

---
+++ neovimSetUP Files/nvim/after/plugin/harpoon.lua
@@ -0,0 +1,16 @@
+local mark = require("harpoon.mark")
+local ui = require("harpoon.mark")
+
+vim.keymap.set("n", "<leader>a", function()
+    require('harpoon.mark').add_file()
+end)
+
+vim.keymap.set("n", "<C-z>", function()
+    require('harpoon.ui').toggle_quick_menu()
+end)
+
+vim.keymap.set("n", "m", function() require('harpoon.ui').nav_file(1) end)
+vim.keymap.set("n", "n", function() require('harpoon.ui').nav_file(2) end)
+vim.keymap.set("n", ",", function() require('harpoon.ui').nav_file(3) end)
+vim.keymap.set("n", ".", function() require('harpoon.ui').nav_file(4) end)
+
